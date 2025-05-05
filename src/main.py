73cd2252e026a48e9394038559c5e0850cb238e1
Jacob Rose <jacob@rostor.cloud>
2025-05-05 18:16:46 +0200

    Added enum for text types

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode, TextType
+
+
+def main():
+    dummy = TextNode("some anchor text", TextType.LINK , "https://www.boot.dev")
+    print(dummy)
+
+if __name__ == "__main__":
+    main()
